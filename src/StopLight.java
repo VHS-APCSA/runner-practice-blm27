13d5cdcec2392db3c3ce07947b8857e6cf2bd5b4
bob <bob@APCSA>
2017-10-16 14:44:06 -0700

    Finished StopLight

--- src/StopLight.java
+++ src/StopLight.java
@@ -17,5 +17,25 @@ public class StopLight
 	public void setColor(Color color) {
 		this.color = color;
 	}
-
+	public void changeLight()
+	{
+		if(color.equals(Color.red))
+		{
+			color = Color.green;
+		}
+		else if(color.equals(Color.green))
+		{
+			color = Color.yellow;
+		}
+		else
+		{
+			color = Color.red;
+		}
+	}
+	public String toString()
+	{
+		String str = "";
+		str = "The color of the stoplight is, " + color;
+		return str;
+	}
 }
--- src/StopLightRunner.java
+++ src/StopLightRunner.java
@@ -1,7 +1,11 @@
 import java.awt.Color;
 public class StopLightRunner {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		StopLight light = new StopLight(Color.green);
+		light.changeLight();
+		System.out.print(light);
 		
 	}
 
