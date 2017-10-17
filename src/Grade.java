deb834d81211e36ced3e6cb376e308a1508b5136
bob <bob@APCSA>
2017-10-17 14:40:57 -0700

    Update grade

--- src/Grade.java
+++ src/Grade.java
@@ -16,5 +16,28 @@ public class Grade
 	public void setGrade(String grade) {
 		this.grade = grade;
 	}
+	public void nextGrade()
+	{
+		if(grade.equals("freshman"))
+		{
+			grade = "sophomore";
+		}
+		else if(grade.equals("sophomore"))
+		{
+			grade = "junior";
+		}
+		else if(grade.equals("junior"))
+		{
+			grade = "senior";
+		}
+		
+	}
+	public int graduate(String mark)
+	{
+		if(mark.equals("A") || )
+		{
+			if(grade.equals("freshman"))
+		}
+	}
 	
 }
